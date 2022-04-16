5224813e278b894c11dda54cb8b05fd1c85a50a1
khadada <lkhadada@gmail.com>
2022-04-16 14:01:18 +0100

    All done with initialize_ui method

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget,QStackedLayout)
 from custom_widget_placeholder import Color
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initialize_ui()
+        self.show()
+    
+    def initialize_ui(self):
+        """
+        Initalize the main window and display its content.
+        """
+        self.setWindowTitle("Stack Layout PyQt5")
+        self.setGeometry(40, 40, 800, 700)
